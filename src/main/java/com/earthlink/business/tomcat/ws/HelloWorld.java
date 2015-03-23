6855de3beb06abb5828f159f03a6fb555d88e7a8
polinchakb <polinchakb@elnk.com>
2015-03-23 16:01:48 -0400

    Spring JAX-WS project that will run in Tomcat 8.

---
+++ src/main/java/com/earthlink/business/tomcat/ws/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.earthlink.business.tomcat.ws;
+
+import java.util.logging.Logger;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+
+@WebService
+public class HelloWorld {
+	
+	private final static Logger logger = Logger.getLogger(HelloWorld.class.getName());
+
+	@WebMethod(operationName = "getHelloWorld")
+	public String getHelloWorld() {
+		logger.info("getHelloWorld invoked.");
+
+		return "Hello";
+
+	}
+}
